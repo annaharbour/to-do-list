411b455ffb4a92574a4f2144e070346ee8b9c84f
Arrowpierced <101281738+Arrowpierced@users.noreply.github.com>
2023-10-19 13:27:25 -0400

    background scaling properly for mobile

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import ToDoList from './ToDoList';
 function App() {
   return (
     <div className="App">
-      <div className='background'></div>
-      <div className='content'>
         <h1>To Do List</h1>
         <ToDoList/>
-      </div>
     </div>
   );
 }
